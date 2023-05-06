1808828b948dbe01ac0f36cf18f2604fe7503de1
Md. Abdus Shohid Shakil <96190677+shakilDevHub@users.noreply.github.com>
2023-05-06 07:09:11 +0600

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ if(notVisited){
     userNameBtn.addEventListener('click', ()=>{
         userName = userNameInp.value;
         nameEnterPage.classList.remove('open');
-        console.log(userName);
         window.localStorage.setItem('visitorName', userName);
     })
 }else{
@@ -48,65 +47,118 @@ openNoteDescription = openNote.querySelector('#openNoteDescription'),
 saveOpenNote = openNote.querySelector('#saveOpenNote'),
 deleteOpenNote = openNote.querySelector('#deleteOpenNote'),
 closeOpenNote = openNote.querySelector('#closeOpenNote'),
-opnNoteImg = openNote.querySelector('#opnNoteImg');
+opnNoteImg = openNote.querySelector('#opnNoteImg'),
+// history
+historyBtn = document.querySelector('.historyBtn'),
+history = openNote.querySelector('.history'),
+historyList = history.querySelector('#historyList'),
+historyListItem = history.querySelector('#historyList li'),
+// History data
+historyData = document.querySelector('.historyData'),
+historyTime = historyData.querySelector('.historyTime'),
+historyDataTitle = historyData.querySelector('h3'),
+historyDataDescription = historyData.querySelector('p'),
+saveHistoryData = historyData.querySelector('.saveHistoryData'),
+closeHistoryData = historyData.querySelector('.closeHistoryData');
 
 const date = new Date();
 let noteList = [];
 
 let defaultPreviewImg = "https://images.unsplash.com/photo-1682687218982-6c508299e107?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80";
 
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
 let sortNotes = (arr)=>{
     arr.sort((a, b)=>{
-        return a[4] - b[4];
+        return a[0][5] - b[0][5];
     })
     return arr;
 }
 // =============================================
 let time = ()=>{
-    month = date.getMonth()+1;
-    year = date.getFullYear();
-    day = date.getDate();
-    return `${month<10? '0'+month: month} | ${day<10? '0'+day: day} | ${year<10? '0'+year : year}`;
+    let month = months[date.getMonth()];
+    let year = date.getFullYear();
+    let day = date.getDate();
+    return `${month} | ${day<10? '0'+day: day} | ${year<10? '0'+year : year}`;
 }
 time();
-
-let getShortName = (name)=>{
-    return name.slice(0, 25)+"....";
+let getFullTime = ()=>{
+    let hour = date.getHours();
+    let amPm = hour > 12 ? 'PM' : 'AM';
+    hour = (hour%12 || 12);
+    hour = (hour < 10) ? ('0' + hour) : hour;
+    let min = date.getMinutes();
+    (min<10) && (min = '0'+min);
+    let sec = date.getSeconds();
+    (sec<10) && (sec = '0'+sec);
+    // console.log(date.getHours());
+    return `${hour} : ${min} : ${sec} ${amPm}`;
 }
+getFullTime();
+
+let getShortName = (nameInp)=>{
+    return nameInp.slice(0, 25)+"....";
+}
+
 let backgroundStyle = (element, url)=>{
     (url=="") && (url = defaultPreviewImg);
+    let testImg = document.querySelector('.testImg');
+    testImg.src = url;
     element.style.backgroundImage = `url(${url})`
+    testImg.addEventListener('error', ()=>{ 
+        element.style.backgroundImage = `url(${defaultPreviewImg})`
+    })
     element.style.backgroundSize = 'cover';
     element.style.backgroundPosition = 'center';
     element.style.backgroundRepeat = 'no-repeat';
 }
-let createNoteHTML = (title, previewImg, date)=>{
+let createNoteHTML = (title, previewImg, date, time)=>{
     let div1 = document.createElement('div');
     let div2 = document.createElement('div');
     let h3 = document.createElement('h3');
-    let h4 = document.createElement('h4');
+    let h41 = document.createElement('h4');
+    let h42 = document.createElement('h4');
     let button = document.createElement('button');
     div1.classList.add('note');
     div2.classList.add('details')
     h3.classList.add('title');
-    h4.classList.add('editedTime');
+    h41.classList.add('editedTime');
+    h42.classList.add('editedTime');
     button.classList.add('openNoteBtn');
     h3.innerText = getShortName(title), 
-    h4.innerText = date;
+    h41.innerText = date;
+    h42.innerText = time;
     button.innerText = 'Open';
     div2.appendChild(h3);
-    div2.appendChild(h4);
+    div2.appendChild(h41);
+    div2.appendChild(h42);
     div2.appendChild(button);
     div1.appendChild(div2);
     notes.appendChild(div1);
     backgroundStyle(div1, previewImg);
     return div1;
 }
+let createHistoryListItem = (fullData)=>{
+    historyList.innerHTML = ' ';
+    // console.table(fullData);
+    fullData.forEach((data)=>{
+        let li = document.createElement('li');
+        let span1 = document.createElement('span');
+        let span2 = document.createElement('span');
+        span1.innerText = data.lastEditDate;
+        span2.innerText = data.lastEditTime;
+        li.appendChild(span1);
+        li.appendChild(span2);
+        historyList.appendChild(li);
+    })
+}
 
 let dataSet = ()=>{
     let getData = JSON.parse((window.localStorage.getItem('noteList')));
+    // console.table(getData);
     return getData;
 }
+
 
 let loadAllNote = ()=>{
     userName = (window.localStorage.getItem('visitorName'));
@@ -116,7 +168,7 @@ let loadAllNote = ()=>{
     noteList = isNaN(Number(getData))? getData: [];
     notes.innerHTML = '';
     for(let i=0; i<noteList.length; i++){
-        createNoteHTML(getData[i][0], getData[i][1], getData[i][3]);
+        createNoteHTML(getData[i][0][0], getData[i][0][1], getData[i][0][3], getData[i][0][4]);
     }
 }
 
@@ -127,6 +179,7 @@ window.addEventListener('load', ()=>{
 addNoteBtn.addEventListener('click', (e)=>{
     e.preventDefault();
     addNote.classList.add('open');
+    addNoteTitle.focus();
     addNoteTitle.value = addNoteDescription.value = addNoteImg.value = "";
 })
 closeNote.addEventListener('click', (e)=>{
@@ -139,6 +192,7 @@ closeOpenNote.addEventListener('click', (e)=>{
     openNote.classList.remove('open');
 })
 
+let indx = 0;
 document.addEventListener('click', (e)=>{
     (!(addNote.contains(e.target)) && (!addNoteBtn.contains(e.target))) && addNote.classList.remove('open');
     
@@ -146,22 +200,26 @@ document.addEventListener('click', (e)=>{
     openNoteBtn.forEach((btn)=>{
         contain *= !(btn.contains(e.target));
     }) 
-    if(!openNote.contains(e.target) && contain){
+    if(!historyData.contains(e.target) && contain && !openNote.contains(e.target)){
         openNote.classList.remove('open');
+        history.classList.remove('open');
+    }
+    if(!historyData.contains(e.target) && !(historyList.contains(e.target))){
+        historyData.classList.remove('open');
     }
     if((e.target).classList.contains('openNoteBtn')){
         openNoteBtn = document.querySelectorAll('.openNoteBtn');
-        let indx = 0;
+        indx = 0;
         openNoteBtn.forEach((btn, i)=>{
             btn.classList.remove('clicked');
             e.target.classList.add('clicked');
             (btn == e.target) && (indx = i);
         })
         noteList = dataSet();
-        let prevData = noteList[indx];
-        openNoteTitle.value = noteList[indx][0];
-        opnNoteImg.value = noteList[indx][1];
-        openNoteDescription.value = noteList[indx][2];
+        let prevData = noteList[indx][0];
+        openNoteTitle.value = noteList[indx][0][0];
+        opnNoteImg.value = noteList[indx][0][1];
+        openNoteDescription.value = noteList[indx][0][2];
         openNote.classList.add('open');
         let verificationOfChanges = ()=>{
             if(prevData[0] != openNoteTitle.value || prevData[1]!=opnNoteImg.value || prevData[2]!=openNoteDescription.value){
@@ -191,20 +249,72 @@ document.addEventListener('click', (e)=>{
             loadAllNote();
             openNote.classList.remove('open');
         })
-        saveOpenNote.addEventListener('click', (e)=>{
-            e.preventDefault();
-            noteList[indx][0] = openNoteTitle.value;
-            noteList[indx][1] = opnNoteImg.value;
-            noteList[indx][2] = openNoteDescription.value;
-            noteList[indx][3] = time();
-            let currentTime = date.getTime();
-            noteList[indx][4] = currentTime;
-            noteList = sortNotes(noteList);
-            window.localStorage.setItem('noteList', JSON.stringify(noteList));
-            loadAllNote();
-            openNote.classList.remove('open');
-        })
     }
+})
+
+let saveOpenNoteFun = (title, imgLink, description)=>{
+    let runFunction = ()=>{
+        let arr = [];
+        arr.push(title);
+        arr.push(imgLink);
+        arr.push(description);
+        arr.push(time());
+        arr.push(getFullTime());
+        let currentTime = date.getTime();
+        console.log(typeof currentTime);
+        arr.push(currentTime);
+        let editedHistory = noteList[indx][1];
+        
+        let editedHistoryData = {
+            data: arr,
+            lastEditDate: arr[3],
+            lastEditTime: arr[4],
+        }
+        editedHistory.push(editedHistoryData);
+        arr = [arr, editedHistory];
+        
+        noteList[indx] = arr;
+        noteList = sortNotes(noteList);
+        window.localStorage.setItem('noteList', JSON.stringify(noteList));
+        loadAllNote();
+    }
+
+    (title!=noteList[indx][0][0] || description!=noteList[indx][0][2]) && runFunction();
+}
+saveOpenNote.addEventListener('click', (e)=>{
+    e.preventDefault();
+    saveOpenNoteFun(openNoteTitle.value, opnNoteImg.value, openNoteDescription.value);
+    saveOpenNote.classList.add('disable');
+})
+
+let allHistoryLi;
+historyBtn.addEventListener('click', ()=>{
+    createHistoryListItem(noteList[indx][1]);
+    allHistoryLi = Array.from(historyList.children);
+    let count = 0;
+    allHistoryLi.forEach((historyLi, i)=>{
+        count = 0;
+        historyLi.addEventListener('click', ()=>{
+            historyData.classList.add('open');
+            historyTime.innerHTML = `<span>${noteList[indx][1][i].lastEditDate}</span>
+            <span>${noteList[indx][1][i].lastEditTime}</span>`;
+            historyDataTitle.innerText = noteList[indx][1][i].data[0]; 
+            historyDataDescription.innerText = noteList[indx][1][i].data[2]; 
+            saveHistoryData.addEventListener('click', (e)=>{
+                e.preventDefault();
+                historyData.classList.remove('open');
+                saveOpenNoteFun(noteList[indx][1][i].data[0], noteList[indx][1][i].data[1], noteList[indx][1][i].data[2], time())
+            })
+            count++;
+            console.log('count == '+count);
+        }, {once: true})
+        console.log('out count == '+count);
+        closeHistoryData.addEventListener('click', (e)=>{
+            e.preventDefault();
+            historyData.classList.remove('open');
+        })
+    })
+    history.classList.toggle('open');
 })
 
 addNoteTitle.value = "", addNoteDescription.value = "";
@@ -218,12 +328,28 @@ addNoteDescription.addEventListener('input', ()=>{
 saveNote.addEventListener('click', (e)=>{
     e.preventDefault();
     let addNoteImgLink = (addNoteImg.value=="")? defaultPreviewImg: addNoteImg.value;
-    let arr = [addNoteTitle.value, addNoteImgLink, addNoteDescription.value, time(), date.getTime()];
+    let arr = [];
+    arr.push(addNoteTitle.value);
+    arr.push(addNoteImgLink);
+    arr.push(addNoteDescription.value);
+    arr.push(time());
+    arr.push(getFullTime());
+    let currentTime = date.getTime();
+    arr.push(currentTime);
+    let editedHistory = [];
+    let editedHistoryData = {
+        data: arr,
+        lastEditDate: arr[3],
+        lastEditTime: arr[4],
+    }
+    editedHistory.push(editedHistoryData);
+    arr = [arr, editedHistory]
+    
     noteList.push(arr);
     noteList = sortNotes(noteList);
     window.localStorage.setItem('noteList', JSON.stringify(noteList));
     addNote.classList.remove('open');
-    createNoteHTML(arr[0], arr[1], arr[3]);
+    loadAllNote();
 })
 clearAllNote.addEventListener('click', (e)=>{
     e.preventDefault();
